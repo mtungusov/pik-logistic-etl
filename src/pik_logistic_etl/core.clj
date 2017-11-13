2d53eb9abb725f99153f15cfd746d8b78ccc86e3
TM <tm@macadmin.ru>
2017-11-13 16:48:18 +0300

    change. add, run-in-thread

--- src/pik_logistic_etl/core.clj
+++ src/pik_logistic_etl/core.clj
@@ -1,31 +1,59 @@
 (ns pik-logistic-etl.core
   (:require [clojure.tools.logging :as log]
             [mount.core :as mount]
-            [pik-logistic-etl.config :refer [settings]]
-            [pik-logistic-etl.db.core :refer [db]]
+            ;[pik-logistic-etl.config :refer [settings]]
+            ;[pik-logistic-etl.db.core :refer [db]]
             [pik-logistic-etl.etl.time-in-zone :as time-in-zone])
   (:gen-class))
 
 (def state (atom {}))
 
 (defn init [args]
-  (log/info "PIK logistic ETL starting...")
   (swap! state assoc :running true)
-  (mount/start #'settings
-               #'db))
+  ;(mount/start #'settings
+  ;             #'db)
+  (mount/start))
 
-(defn stop []
-  (swap! state assoc :running false)
+
+(defn- stop-main-loop []
+  (swap! state assoc :running false))
+
+
+(defn- stop []
   (log/info "Stopping...")
-  (shutdown-agents)
-  (Thread/sleep 1000)
-  (log/info "Stopped!"))
+  (stop-main-loop)
+  (log/info "Stopped."))
+
+
+(defn- run-in-thread [period f]
+  (.start
+    (Thread.
+      (fn []
+        (try
+          (while (:running @state)
+            (f)
+            (Thread/sleep period))
+          (catch InterruptedException _)
+          (catch Exception e
+            (log/info e)
+            (stop-main-loop)))))))
+
+
+(defn- etl-time-in-zone []
+  (log/info "ETL time-in-zone Start")
+  (time-in-zone/process)
+  (log/info "ETL time-in-zone Finish"))
+
 
 (defn -main [& args]
   (init args)
   (.addShutdownHook (Runtime/getRuntime)
                     (Thread. stop))
-  (while (:running @state)
-    (log/info "ETL time-in-zone")
-    (time-in-zone/process)
-    (Thread/sleep (* 15 60 1000))))
+  (log/info "PIK logistic ETL starting...")
+
+  (run-in-thread (* 15 60 1000) etl-time-in-zone)
+
+  (try
+    (while (:running @state)
+      (Thread/sleep 1000))
+    (catch InterruptedException e)))
